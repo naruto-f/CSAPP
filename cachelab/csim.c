55caa0f0462d91842e966aadc276c4f9eb92e948
naruto-f <476971219@qq.com>
2024-04-15 22:24:52 +0800

    cache lab完成  利用block的思想来增加缓存的利用率还是比较有意思的但也很烧脑。

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -1,7 +1,242 @@
+#define _GNU_SOURCE
 #include "cachelab.h"
 
-int main()
+#include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <math.h>
+
+struct Cache_Node {
+    struct Cache_Node *front;
+    struct Cache_Node *next;
+    unsigned long long tag;
+    char *offset;
+};
+
+struct LRU_Cache {
+    struct Cache_Node *begin;
+    struct Cache_Node *end;
+    struct Cache_Node *idle_list;
+};
+
+struct LRU_Cache **cache;
+int hits, misses, evictions;
+int tag_bit_len, set_bit_len, block_offset_bit_len, line_num_peer_set;
+int verbose;
+
+uint64_t convert_to_addr_ll(char* buf) {
+    uint64_t ret = 0;
+    int len = strlen(buf);
+    //buf[len] = 32;
+
+    for (int i = len - 1; i >= 0; --i) {
+        char c = buf[i];
+        if (c >= '0' && c <= '9') {
+            c = c - '0';
+        } else {
+            c = (c - 'a') + 10;
+        }
+
+        ret = ret | ((uint64_t)c << ((len - 1 - i) * 4));
+    }
+
+    return ret;
+}
+
+struct Cache_Node* init_cache_node() {
+    struct Cache_Node* node = (struct Cache_Node*) malloc(sizeof(struct Cache_Node));
+    memset(node, '\0', sizeof(struct Cache_Node));
+    return node;
+}
+
+struct LRU_Cache* init_one_cache_set() {
+    struct LRU_Cache* lru_cache = (struct LRU_Cache*) malloc(sizeof(struct LRU_Cache));
+    lru_cache->begin = init_cache_node();
+    lru_cache->end = init_cache_node();
+    lru_cache->begin->next = lru_cache->end;
+    lru_cache->end->front = lru_cache->begin;
+
+    for (int i = 0; i < line_num_peer_set; ++i) {
+        struct Cache_Node* node = init_cache_node();
+        if (i == 0) {
+            lru_cache->idle_list = node;
+        } else {
+            node->next = lru_cache->idle_list;
+            lru_cache->idle_list->front = node;
+            lru_cache->idle_list = node;
+        }
+    }
+
+    return lru_cache;
+}
+
+void init_cache() {
+    size_t malloc_size = pow(2, set_bit_len) * sizeof(struct LRU_Cache*);
+    cache = (struct LRU_Cache**) malloc(malloc_size);
+    memset(cache, '\0', malloc_size);
+
+    for (int i = 0; i < pow(2, set_bit_len); ++i) {
+        cache[i] = init_one_cache_set();
+    }
+}
+
+struct Cache_Node* cache_line_exist(uint64_t tag, struct LRU_Cache* lru_cache) {
+    struct Cache_Node* node = lru_cache->begin->next;
+
+    for (; node != lru_cache->end; node = node->next) {
+        if (node->tag == tag) {
+            return node;
+        }
+    }
+    return NULL;
+}
+
+void insert_before_node(struct Cache_Node* target_node, struct Cache_Node* insert_node) {
+    insert_node->front = target_node->front;
+    target_node->front->next = insert_node;
+    target_node->front = insert_node;
+    insert_node->next = target_node;
+}
+
+struct Cache_Node* delete_node(struct Cache_Node* delete_node) {
+    delete_node->front->next = delete_node->next;
+    delete_node->next->front = delete_node->front;
+    delete_node->front = NULL;
+    delete_node->next = NULL;
+    return delete_node;
+}
+
+void fetch_cache_block(uint64_t tag, uint64_t set) {
+    struct LRU_Cache* lru_cache = cache[set];
+    struct Cache_Node* node = cache_line_exist(tag, lru_cache);
+    if (!node) {
+        ++misses;
+        if (verbose) {
+            printf("miss ");
+        }
+
+        if (!lru_cache->idle_list) {
+            ++evictions;
+            if (verbose) {
+                printf("eviction ");
+            }
+
+            node = lru_cache->begin->next;
+            delete_node(node);
+        } else {
+            node = lru_cache->idle_list;
+            lru_cache->idle_list = lru_cache->idle_list->next;
+            if (lru_cache->idle_list) {
+                lru_cache->idle_list->front = NULL;
+            }
+            node->next = NULL;
+        }
+        node->tag = tag;
+        insert_before_node(lru_cache->end, node);
+    } else {
+        ++hits;
+        if (verbose) {
+            printf("hit ");
+        }
+        delete_node(node);
+        insert_before_node(lru_cache->end, node);
+    }
+}
+
+void process_one_line(const char* line) {
+    if (line[0] == 'I') {
+        return;
+    }
+
+    char op = 0;
+    char buf[9] = { '\0' };
+    int offset = 0;
+    uint64_t address = 0;
+
+    if (sscanf(line, " %c %[^,],%d\n", &op, buf, &offset) == -1) {
+        exit(1);
+    }
+
+    address = convert_to_addr_ll(buf);
+    uint64_t tag = address & ~(UINT64_MAX >> tag_bit_len);
+    uint64_t set = (address & (UINT64_MAX >> tag_bit_len) & (UINT64_MAX << block_offset_bit_len)) >> block_offset_bit_len;
+
+    if (verbose) {
+        printf("%s", line);
+    }
+
+    switch (op) {
+        case 'L':
+            fetch_cache_block(tag, set);
+            break;
+        case 'S':
+            fetch_cache_block(tag, set);
+            break;
+        case 'M':
+            fetch_cache_block(tag, set);
+            ++hits;
+            if (verbose) {
+                printf("hit ");
+            }
+            break;
+        default:
+            break;
+    }
+
+    if (verbose) {
+        printf("\n");
+    }
+}
+
+int main(int argc, char *argv[])
 {
-    printSummary(0, 0, 0);
+    int opt = -1;
+    FILE *trace_file = NULL;
+    char *line = NULL;
+    size_t line_len = 0;
+    ssize_t getline_ret = -1;
+
+    // 选项字符串定义了程序接受的选项，冒号表示该选项需要一个参数
+    while ((opt = getopt(argc, argv, "s:E:b:t:v")) != -1) {
+        switch (opt) {
+            case 's':
+                set_bit_len = atoi(optarg);
+                break;
+            case 'E':
+                line_num_peer_set = atoi(optarg);
+                break;
+            case 'b':  // 不带参数的选项
+                block_offset_bit_len = atoi(optarg);
+                break;
+            case 't':
+                trace_file = fopen(optarg, "r");
+                if (trace_file == NULL) {
+                    perror("打开文件失败");
+                    return 1;
+                }
+                break;
+            case 'v':
+                verbose = 1;
+                break;
+            default:
+                break;
+        }
+    }
+    tag_bit_len = 64 - block_offset_bit_len - set_bit_len;
+
+    init_cache();
+
+    while (1) {
+        getline_ret = getline(&line, &line_len, trace_file);
+        if (getline_ret == -1) {
+            break;
+        }
+        process_one_line(line);
+    }
+
+    printSummary(hits, misses, evictions);
     return 0;
 }
--- cachelab/trans.c
+++ cachelab/trans.c
@@ -8,7 +8,10 @@
  * on a 1KB direct mapped cache with a block size of 32 bytes.
  */ 
 #include <stdio.h>
+#include <math.h>
 #include "cachelab.h"
+
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
@@ -20,9 +23,127 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     be graded. 
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+
 }
+
+char transpose_61x67_desc[] = "Transpose submission61x67";
+void transpose_61x67_submit(int M, int N, int A[N][M], int B[M][N]) {
+    int stride = 16;
+
+    for (int kk = 0; kk < N; kk += stride) {
+        for (int ll = 0; ll < M; ll += stride) {
+            for (int i = kk; i < min(kk + stride, N); ++i) {
+                for (int j = ll; j < min(ll + stride, M); ++j) {
+                    B[j][i] = A[i][j];
+                }
+            }
+        }
+    }
+
+}
+
+char transpose_32x32_desc[] = "Transpose submission32x32";
+void transpose_32x32_submit(int M, int N, int A[N][M], int B[M][N])
+{
+    int tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, tmp8;
+    // int row_stride = 8, col_stride = 8;
+    int stride = 8;
+
+    for (int kk = 0; kk < N; kk += stride) {
+        for (int ll = 0; ll < M; ll += stride) {
+            for (int i = kk; i < kk + stride; ++i) {
+                for (int j = ll; j < ll + stride; j += stride) {
+                    /* 因为模拟缓存是使用直接映射，数据A和B位于对角线上的块由偏移相同所以会映射到同一个缓存块，所以位于数组B的读取可能会覆盖之前读取且需要使用的数组A内容的缓存行，所以可以使用8个局部变量缓存可能被覆盖的A的数据 */
+                    tmp1 = A[i][j];
+                    tmp2 = A[i][j + 1];
+                    tmp3 = A[i][j + 2];
+                    tmp4 = A[i][j + 3];
+                    tmp5 = A[i][j + 4];
+                    tmp6 = A[i][j + 5];
+                    tmp7 = A[i][j + 6];
+                    tmp8 = A[i][j + 7];
+
+                    B[j][i] = tmp1;
+                    B[j + 1][i] = tmp2;
+                    B[j + 2][i] = tmp3;
+                    B[j + 3][i] = tmp4;
+                    B[j + 4][i] = tmp5;
+                    B[j + 5][i] = tmp6;
+                    B[j + 6][i] = tmp7;
+                    B[j + 7][i] = tmp8;
+                }
+            }
+        }
+    }
+}
+
+
+char transpose_64x64_desc[] = "Transpose submission64*64";
+void transpose_64x64_submit(int M, int N, int A[N][M], int B[M][N])
+{
+    int tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, tmp8;
+
+    //将8x8的大块分成4x4的小块处理
+    for (int kk = 0; kk < N; kk += 8) {
+        for (int ll = 0; ll < M; ll += 8) {
+            for (int i = kk; i < kk + 4; ++i) {
+                //使用局部变量保存A当前在缓存中的行
+                tmp1 = A[i][ll];
+                tmp2 = A[i][ll + 1];
+                tmp3 = A[i][ll + 2];
+                tmp4 = A[i][ll + 3];
+                tmp5 = A[i][ll + 4];
+                tmp6 = A[i][ll + 5];
+                tmp7 = A[i][ll + 6];
+                tmp8 = A[i][ll + 7];
+
+                //赋值给B的1和2块，核心还是要充分利用已经在缓存上的内容
+                B[ll][i] = tmp1;
+                B[ll + 1][i] = tmp2;
+                B[ll + 2][i] = tmp3;
+                B[ll + 3][i] = tmp4;
+                B[ll][i + 4] = tmp5;
+                B[ll + 1][i + 4] = tmp6;
+                B[ll + 2][i + 4] = tmp7;
+                B[ll + 3][i + 4] = tmp8;
+            }
+
+            for (int i = ll; i < ll + 4; ++i) {
+                tmp1 = B[i][kk + 4];
+                tmp2 = B[i][kk + 5];
+                tmp3 = B[i][kk + 6];
+                tmp4 = B[i][kk + 7];
+
+                //转置A的第3块到B的第2块
+                B[i][kk + 4] = A[kk + 4][i];
+                B[i][kk + 5] = A[kk + 5][i];
+                B[i][kk + 6] = A[kk + 6][i];
+                B[i][kk + 7] = A[kk + 7][i];
+
+                //将原先保存在B的第2块中的内容平移到B的第三块
+                B[i + 4][kk + 0] = tmp1;
+                B[i + 4][kk + 1] = tmp2;
+                B[i + 4][kk + 2] = tmp3;
+                B[i + 4][kk + 3] = tmp4;
+            }
+            for (int i = kk + 4; i < kk + 8; ++i) {
+                //转置A的第4块到B的第4块，因为对角线上的数据块缓存映射冲突，所以先将A中的行保存到4个局部变量中。
+                tmp1 = A[i][ll + 4];
+                tmp2 = A[i][ll + 5];
+                tmp3 = A[i][ll + 6];
+                tmp4 = A[i][ll + 7];
+
+                B[ll + 4][i] = tmp1;
+                B[ll + 5][i] = tmp2;
+                B[ll + 6][i] = tmp3;
+                B[ll + 7][i] = tmp4;
+            }
+        }
+    }
+}
+
+
 
 /* 
  * You can define additional transpose functions below. We've defined
@@ -42,7 +163,7 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
+    }
 
 }
 
@@ -56,11 +177,14 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    registerTransFunction(trans, trans_desc);
 
+    registerTransFunction(transpose_64x64_submit, transpose_64x64_desc);
+    registerTransFunction(transpose_32x32_submit, transpose_32x32_desc);
+    registerTransFunction(transpose_61x67_submit, transpose_61x67_desc);
 }
 
 /* 
