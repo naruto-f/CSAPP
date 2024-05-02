42ab73784f00f85081e3438e62c28b2f8ce8a9b6
naruto-f <476971219@qq.com>
2024-05-03 00:06:20 +0800

    malloc第二版，使用分离链表(双链表) + FILO(头插法) + 首次匹配 + 聚合相邻空闲块，平均性能指标Perf index = 45 (util) + 40 (thru) = 85/100

--- malloclab/mm.c
+++ malloclab/mm.c
@@ -49,15 +49,39 @@ team_t team = {
 
 #define GET(address) (*(unsigned int *)(address))
 
-#define DATA_PTR(address) ((char *)(address) + 8)
+#define GET_ALLOC(address) (GET(address) & 0x1)
+
+#define GET_SIZE(address) (GET(address) & ~0x7)
+
+#define PACK(size, alloc) (size | alloc)
+
+#define DATA_PTR(address) ((char *)(address) + 12)
 
 #define SUCC_PTR(address) ((char *)(address) + 4)
 
-#define BLOCK_START_PTR(address) ((char *)(address) - 8)
+#define PREV_PTR(address) ((char *)(address) + 8)
+
+#define BLOCK_START_PTR(address) ((char *)(address) - 12)
+
+#define NEXT_BLKP(ptr) ((char*)(ptr) + GET_SIZE(ptr))
+
+#define PREV_BLKP(ptr) ((char*)(ptr) - GET_SIZE(((char*)ptr) - 4))
+
+#define FT_PTR(ptr) ((char*)(ptr) + GET_SIZE(ptr) - 4)
+
+#define CONTROL_DATA_LEN 16
 
 #define EXTEND_PAGE_NUM 1
 
+#define FIRST_MATCH 0
+
+#define BEST_MATCH 1
+
+#define MIN_ADDR_MATCH 2
+
 static unsigned int **free_lists;
+
+static int match_method = FIRST_MATCH;
 
 /* 线性时间复杂度版本，后面可以优化为二分查找 */
 static int caculate_largest_power_of_two_lessthan_n(int n) {
@@ -78,12 +102,12 @@ static int caculate_largest_power_of_two_lessthan_n(int n) {
     } while (1);
 }
 
-static void *try_find_in_freelist(int free_list_index, size_t size, unsigned int **address_of_list_front) {
+static void *find_by_first_match(int free_list_index, size_t size, unsigned int **address_of_list_front) {
     unsigned int *pre = (unsigned int *)free_lists + free_list_index;
     unsigned int *cur = free_lists[free_list_index];
     uintptr_t succ_address = 0;
 
-    while (GET(cur) < size) {
+    while (GET_SIZE(cur) < size) {
         pre = cur;
         if (GET(SUCC_PTR(cur)) == 0) {
             return NULL;
@@ -98,24 +122,125 @@ static void *try_find_in_freelist(int free_list_index, size_t size, unsigned int
     return cur;
 }
 
+static void *find_by_best_match(int free_list_index, size_t size, unsigned int **address_of_list_front) {
+    unsigned int *pre = (unsigned int *)free_lists + free_list_index;
+    unsigned int *cur = free_lists[free_list_index];
+    unsigned int *ret = NULL;
+    unsigned int min_mul = UINT32_MAX;
+    uintptr_t succ_address = 0;
+
+    while (1) {
+        unsigned int cur_block_size = GET_SIZE(cur);
+        if (cur_block_size >= size) {
+            if (cur_block_size == size) {
+                ret = cur;
+                break;
+            } else {
+                if (cur_block_size - size < min_mul) {
+                    min_mul = cur_block_size - size;
+                    ret = cur;
+                }
+            }
+        }
+
+
+        if (GET(SUCC_PTR(cur)) == 0) {
+            break;
+        } else {
+            succ_address = (uintptr_t)*(unsigned int *)SUCC_PTR(cur);
+            pre = cur;
+            //succ_address = (uintptr_t)SUCC_PTR(cur);
+            cur = (unsigned int *)succ_address;
+        }
+    }
+
+    *address_of_list_front = pre;
+    return ret;
+}
+
+static void *find_by_min_addr_match(int free_list_index, size_t size, unsigned int **address_of_list_front) {
+    unsigned int *pre = (unsigned int *)free_lists + free_list_index;
+    unsigned int *cur = free_lists[free_list_index];
+    unsigned int *ret = NULL;
+    uintptr_t succ_address = 0;
+
+    while (1) {
+        unsigned int cur_block_size = GET_SIZE(cur);
+        if (cur_block_size >= size) {
+            if (cur < ret) {
+                ret = cur;
+            }
+        }
+
+        if (GET(SUCC_PTR(cur)) == 0) {
+            break;
+        } else {
+            succ_address = (uintptr_t)*(unsigned int *)SUCC_PTR(cur);
+            pre = cur;
+            //succ_address = (uintptr_t)SUCC_PTR(cur);
+            cur = (unsigned int *)succ_address;
+        }
+    }
+
+    *address_of_list_front = pre;
+    return ret;
+}
+
+
+static int in_free_lists(void *ptr) {
+    if ((char*)ptr >= (char*)free_lists && (char*)ptr < (char*)free_lists + 4 * 32) {
+        return 1;
+    }
+    return 0;
+}
+
+
+
+static void *try_find_in_freelist(int free_list_index, size_t size, unsigned int **address_of_list_front) {
+    void *ret = NULL;
+
+    switch (match_method) {
+        case FIRST_MATCH:
+            ret = find_by_first_match(free_list_index, size, address_of_list_front);
+            break;
+        case BEST_MATCH:
+            ret = find_by_best_match(free_list_index, size, address_of_list_front);
+            break;
+        case MIN_ADDR_MATCH:
+            ret = find_by_min_addr_match(free_list_index, size, address_of_list_front);
+            break;
+        default:
+            break;
+    }
+    return ret;
+}
+
 static void delete_block_from_free_list(void *block_start, void *front_block_in_list) {
     uintptr_t address = (uintptr_t)GET(SUCC_PTR(block_start));
-    if ((char*)front_block_in_list >= (char*)free_lists && (char*)front_block_in_list < (char*)free_lists + 4 * 32) {
+    if (in_free_lists(front_block_in_list)) {
         PUT(front_block_in_list, address);
     } else {
         PUT(SUCC_PTR(front_block_in_list), address);
     }
 
+    if (address) {
+        PUT(PREV_PTR((unsigned *)address), (uintptr_t)front_block_in_list);
+    }
+
     PUT(SUCC_PTR(block_start), 0);
+    PUT(PREV_PTR(block_start), 0);
 }
 
 static void insert_block_to_free_list(void *block_start, int free_list_index) {
     PUT(SUCC_PTR(block_start), 0);
+    PUT(PREV_PTR(block_start), 0);
     uintptr_t addr = (uintptr_t)block_start;
     if (free_lists[free_list_index] != NULL) {
         uintptr_t temp_addr = free_lists[free_list_index];
         PUT(SUCC_PTR(block_start), temp_addr);
+        PUT(PREV_PTR(temp_addr), (uintptr_t)block_start);
     }
+    PUT(PREV_PTR(block_start), (uintptr_t)((unsigned int*)free_lists + free_list_index));
     //PUT(&free_lists[free_list_index], addr);
     free_lists[free_list_index] = (unsigned int *)addr;
 }
@@ -123,10 +248,11 @@ static void insert_block_to_free_list(void *block_start, int free_list_index) {
 static void insert_block_to_fitting_free_list(void *block_start, size_t block_size) {
     int free_list_index = caculate_largest_power_of_two_lessthan_n(block_size);
     insert_block_to_free_list(block_start, free_list_index);
-    PUT(block_start, block_size);
+    PUT(block_start, PACK(block_size, 0));
+    PUT(FT_PTR(block_start), PACK(block_size, 0));
 }
 
-static void *extend_mem_for_free_list(int free_list_index) {
+static void *extend_mem_for_free_list(int free_list_index, int newsize) {
     int block_size = ALIGN((int)pow(2, free_list_index));
     int block_num = ALIGN(EXTEND_PAGE_NUM * mem_pagesize()) / block_size - 1;
     int extend_size = ALIGN(EXTEND_PAGE_NUM * mem_pagesize());
@@ -136,6 +262,12 @@ static void *extend_mem_for_free_list(int free_list_index) {
         block_num = 0;
     }
 
+//    if (newsize > extend_size) {
+//        extend_size = newsize;
+//        block_size = extend_size;
+//        block_num = 0;
+//    }
+
     void *new_extend_base = mem_sbrk(extend_size);
     if (new_extend_base == (void *)-1) {
         return NULL;
@@ -143,12 +275,16 @@ static void *extend_mem_for_free_list(int free_list_index) {
 
     char *ptr = (char *)new_extend_base + block_size;
     for (int i = 0; i < block_num; ++i) {
-        PUT(ptr, block_size);
+        PUT(ptr, PACK(block_size, 0));
+        PUT(FT_PTR(ptr), PACK(block_size, 0));
         insert_block_to_free_list(ptr, free_list_index);
         ptr += block_size;
     }
 
-    PUT(new_extend_base, block_size);
+    PUT(new_extend_base, PACK(block_size, 1));
+    PUT(FT_PTR(new_extend_base), PACK(block_size, 1));
+    PUT(PREV_PTR(new_extend_base), 0);
+    PUT(SUCC_PTR(new_extend_base), 0);
     return new_extend_base;
 }
 
@@ -158,7 +294,7 @@ static void *extend_mem_for_free_list(int free_list_index) {
  */
 int mm_init(void)
 {
-    if ((free_lists = mem_sbrk(32 * 4)) == (void *)-1) {
+    if ((free_lists = mem_sbrk(33 * 4)) == (void *)-1) {
         return -1;
     }
 
@@ -181,38 +317,51 @@ void *mm_malloc(size_t size)
         return NULL;
     }
 
-    if (size == 16) {
+    if (1) {
         ++debug_count;
     }
 
-    int newsize = ALIGN(size + 8);
+    int newsize = ALIGN(size + CONTROL_DATA_LEN);
     int free_list_index = caculate_largest_power_of_two_lessthan_n(newsize);
     void *ptr = NULL;
     unsigned int *front_address_ptr = NULL;
+    int old_free_list_index = free_list_index;
 
-    if (free_lists[free_list_index] != NULL) {
-        //如果当前空闲链表中有空闲块则尝试在链表中寻找第一个合适的空闲块
-        ptr = try_find_in_freelist(free_list_index, newsize, &front_address_ptr);
-        if (ptr != NULL) {
-            size_t block_size = GET(ptr);
-            if (block_size - newsize > 8) {
-                insert_block_to_fitting_free_list((char *)ptr + newsize, block_size - newsize);
-                block_size = newsize;
+    while (free_list_index <= 31) {
+        if (free_lists[free_list_index] != NULL) {
+            //如果当前空闲链表中有空闲块则尝试在链表中寻找第一个合适的空闲块
+            ptr = try_find_in_freelist(free_list_index, newsize, &front_address_ptr);
+            if (ptr != NULL) {
+                size_t block_size = GET_SIZE(ptr);
+                int need_partion = 0;
+                size_t partion_size = 0;
+                if (block_size - newsize > 16) {
+                    need_partion = 1;
+                    partion_size = block_size - newsize;
+                    block_size = newsize;
+                }
+
+                PUT(ptr, PACK(block_size, 1));
+                PUT(FT_PTR(ptr), PACK(block_size, 1));
+                delete_block_from_free_list(ptr, front_address_ptr);
+
+                if (need_partion) {
+                    insert_block_to_fitting_free_list((char *)ptr + newsize, partion_size);
+                }
+
+                if ((char*)ptr + block_size - 1 > (char*)mem_heap_hi()) {
+                    printf("heap overflow\n");
+                }
+
+                return DATA_PTR(ptr);
             }
-
-            PUT(ptr, block_size);
-            delete_block_from_free_list(ptr, front_address_ptr);
-
-            if ((char*)ptr + block_size - 1 > (char*)mem_heap_hi()) {
-                printf("heap overflow\n");
-            }
-
-            return DATA_PTR(ptr);
         }
+        ++free_list_index;
     }
 
+
     //如果空闲链表中没有任何空闲块或没有合适的空闲块则为该空闲链表分配一批大小为当前空闲链表最大能存放的空闲块并取一块给用户
-    void *new_extend_base = extend_mem_for_free_list(free_list_index);
+    void *new_extend_base = extend_mem_for_free_list(old_free_list_index, newsize);
     if (new_extend_base == NULL) {
         return NULL;
     }
@@ -224,6 +373,57 @@ void *mm_malloc(size_t size)
     return DATA_PTR(new_extend_base);
 }
 
+static void delete_block_for_coalesce(void *ptr) {
+    uintptr_t addr = 0;
+    addr = GET(PREV_PTR(ptr));
+    PUT(FT_PTR(ptr), 0);
+    PUT(ptr, 0);
+    delete_block_from_free_list(ptr, (void*)addr);
+}
+
+static void *coalesce(void *ptr) {
+    size_t prev_alloc = 1;
+    size_t next_alloc = 1;
+    size_t size = GET_SIZE(ptr);
+    void *last_header_ptr = ptr;
+    uintptr_t addr = 0;
+
+    if ((char*)ptr <= mem_heap_lo() + 33 * 4) {
+        prev_alloc = 1;
+    } else {
+        prev_alloc = GET_ALLOC(PREV_BLKP(ptr));
+    }
+
+    if (NEXT_BLKP(ptr) < (char*)mem_heap_hi() + 1) {
+        next_alloc = GET_ALLOC(NEXT_BLKP(ptr));
+    } else {
+        next_alloc = 1;
+    }
+
+    if (prev_alloc && next_alloc) {
+        return ptr;
+    } else if (prev_alloc && !next_alloc) {
+        size += GET_SIZE(NEXT_BLKP(ptr));
+        delete_block_for_coalesce(NEXT_BLKP(ptr));
+    } else if (!prev_alloc && next_alloc) {
+        last_header_ptr = PREV_BLKP(ptr);
+        size += GET_SIZE(PREV_BLKP(ptr));
+        delete_block_for_coalesce(PREV_BLKP(ptr));
+    } else {
+        last_header_ptr = PREV_BLKP(ptr);
+        size += GET_SIZE(PREV_BLKP(ptr));
+        size += GET_SIZE(NEXT_BLKP(ptr));
+        delete_block_for_coalesce(PREV_BLKP(ptr));
+        delete_block_for_coalesce(NEXT_BLKP(ptr));
+    }
+
+    delete_block_for_coalesce(ptr);
+    PUT(last_header_ptr, size);
+    PUT(FT_PTR(last_header_ptr), size);
+    insert_block_to_fitting_free_list(last_header_ptr, size);
+    return last_header_ptr;
+}
+
 /*
  * mm_free - Freeing a block does nothing.
  */
@@ -233,8 +433,9 @@ void mm_free(void *ptr)
         return;
     }
 
-    size_t block_size = GET(BLOCK_START_PTR(ptr));
+    size_t block_size = GET_SIZE(BLOCK_START_PTR(ptr));
     insert_block_to_fitting_free_list(BLOCK_START_PTR(ptr), block_size);
+    coalesce(BLOCK_START_PTR(ptr));
 }
 
 /*
@@ -253,12 +454,12 @@ void *mm_realloc(void *ptr, size_t size)
 
     void *oldptr = ptr;
     void *newptr;
-    size_t block_size = GET(BLOCK_START_PTR(ptr));
-    size_t new_block_size = ALIGN(size + SIZE_T_SIZE);
+    size_t block_size = GET_SIZE(BLOCK_START_PTR(ptr));
+    size_t new_block_size = ALIGN(size + CONTROL_DATA_LEN);
     size_t copySize = 0;
 
     if (block_size >= new_block_size) {
-//        if (block_size - new_block_size > 8) {
+//        if (block_size - new_block_size > 16) {
 //            PUT(BLOCK_START_PTR(ptr), new_block_size);
 //            insert_block_to_fitting_free_list((char *)oldptr + new_block_size, block_size - new_block_size);
 //        }
@@ -270,7 +471,7 @@ void *mm_realloc(void *ptr, size_t size)
             return NULL;
         }
 
-        copySize = block_size - 4;
+        copySize = block_size - CONTROL_DATA_LEN;
         memcpy(newptr, oldptr, copySize);
         mm_free(oldptr);
         return newptr;
